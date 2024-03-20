5da08ecce5c70810d04443df53535f0a53ae8263
Pham Van Tung <Phamtungkp2002@gmail.com>
2024-03-20 10:34:06 +0700

    TSubject

--- src/main/java/trainingmanagement/repository/SubjectRepository.java
+++ src/main/java/trainingmanagement/repository/SubjectRepository.java
@@ -4,11 +4,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import trainingmanagement.model.entity.Subject;
+import trainingmanagement.model.enums.EActiveStatus;
+
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface SubjectRepository extends JpaRepository<Subject, Long> {
     List<Subject> findBySubjectNameContainingIgnoreCase(String subjectName);
     @Query(value = "select s.* from subject s join class_subject cs on s.id=cs.subject_id where cs.class_id=:classId",nativeQuery = true)
     List<Subject> getAllByClassId(Long classId);
+    List<Subject> getAllByStatus(EActiveStatus status);
+    Optional<Subject> findByIdAndStatus(Long subjectId, EActiveStatus status);
 }
